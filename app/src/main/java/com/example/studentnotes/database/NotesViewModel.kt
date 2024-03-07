e14d24fa63cbd5cecfc4facea22063a7dd54e34b
CEF2003 <124346261+CEF2003@users.noreply.github.com>
2024-03-07 22:57:16 +0200

    Third commit

--- app/src/main/java/com/example/studentnotes/database/NotesViewModel.kt
+++ app/src/main/java/com/example/studentnotes/database/NotesViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.studentnotes.database
 
-import android.content.Context
-import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
